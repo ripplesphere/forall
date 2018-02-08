83e751434b8f1220ada6b0d50e89a2ade8c282dd
George <robertmills339@gmail.com>
2018-02-08 09:41:11 -0500

    Setting up edeliver

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Forall.Mixfile do
   def application do
     [
       mod: {Forall.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :edeliver]
     ]
   end
 
@@ -40,7 +40,9 @@ defmodule Forall.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:edeliver, "~> 1.4.2"},
+      {:distillery, "~> 1.4"}
     ]
   end
 
