0d9b008f61b82612c88358e14687d89784e6fcdd
George <robertmills339@gmail.com>
2018-02-09 14:16:01 -0500

    More changes for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -43,8 +43,8 @@ config :logger, level: :info
 # We also recommend setting `force_ssl`, ensuring no data is
 # ever sent via http, always redirecting to https:
 #
-#     config :forall, ForallWeb.Endpoint,
-#       force_ssl: [hsts: true]
+config :forall, ForallWeb.Endpoint,
+  force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
